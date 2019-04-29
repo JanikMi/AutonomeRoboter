8a5a2e26cde1d7e078e1188dba2b3516caa35cc5
JanikMi <janik.mildenberger@yahoo.de>
2019-04-29 13:33:52 +0200

    neuste Version

--- src/turtlebot_highlevel_controller_node.cpp
+++ src/turtlebot_highlevel_controller_node.cpp
@@ -2,22 +2,20 @@
 #include "std_msgs/String.h"
 #include "turtlebot_highlevel_controller/TurtlebotHighlevelController.hpp"
 #include "sensor_msgs/LaserScan.h"
+#include <sstream>
 
 void chatterCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
   //ROS_INFO("I heard: [%s]", msg->data.c_str());
-  ROS_INFO("I heard: [%s]", msg->range_min);
+  ROS_INFO("Ausgabe: [%s]", msg->range_min);
   
 }
 
-
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "turtlebot_highlevel_controller");
-  //ros::NodeHandle nodeHandle("~");
-  ros::NodeHandle n;
-
-  ros::Subscriber sub = n.subscribe("scan", 10, chatterCallback);
+  ros::init(argc, argv, "listener");
+  ros::NodeHandle mynode;
+  ros::Subscriber subscriber = mynode.subscribe("scan", 3, chatterCallback);
   //turtlebot_highlevel_controller::TurtlebotHighlevelController TurtlebotHighlevelController(nodeHandle);
 
   ros::spin();
