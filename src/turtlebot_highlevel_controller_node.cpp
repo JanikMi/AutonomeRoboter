2d4aec452d56eaa07578f37c02027196ed050750
JanikMi <janik.mildenberger@yahoo.de>
2019-05-01 16:06:45 +0200

    Parameter aus yaml file werden in cpp geladen

--- src/turtlebot_highlevel_controller_node.cpp
+++ src/turtlebot_highlevel_controller_node.cpp
@@ -3,6 +3,8 @@
 #include "turtlebot_highlevel_controller/TurtlebotHighlevelController.hpp"
 #include "sensor_msgs/LaserScan.h"
 #include <sstream>
+#include <ros/param.h>
+#include <iostream>
 
 void chatterCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
@@ -15,7 +17,21 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "listener");
   ros::NodeHandle mynode;
-  ros::Subscriber subscriber = mynode.subscribe("scan", 3, chatterCallback);
+
+  int queue_size;
+  std::string topic;
+  if(!mynode.getParam("/_subscriber/queue_size", queue_size))
+  {
+      ROS_ERROR_STREAM("queue_size wurde nicht gelesen.");
+      return 0;
+  }
+  if(!mynode.getParam("/_subscriber/topic", topic))
+  {
+      ROS_ERROR_STREAM("topic wurde nicht gelesen.");
+      return 0;
+  }
+
+  ros::Subscriber subscriber = mynode.subscribe(topic, queue_size, chatterCallback);
   //turtlebot_highlevel_controller::TurtlebotHighlevelController TurtlebotHighlevelController(nodeHandle);
 
   ros::spin();
